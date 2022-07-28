aef61fcec589330d01b58c3338e1503050c3bf06
bleeptrack <mail@bleeptrack.de>
2022-07-28 15:30:43 +0200

    first interaction running

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+let stageID = undefined
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -15,10 +16,27 @@ app.get('/stage', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  if(stageID){
+      let msg = {
+         ID: socket.id,
+         word: "LAMA"
+      }
 
-  socket.on('hello', (msg) => {
+      io.to(stageID).emit("join", msg);
+    }
+
+  socket.on('move', (msg) => {
     console.log('message: ' + msg);
+    if(stageID){
+        msg.ID = socket.id
+        io.to(stageID).emit("move", msg);
+    }
   });
+
+  socket.on("stage announcement", function() {
+      stageID = socket.id
+      console.log("new stage id set: ", socket.id)
+  })
 });
 
 server.listen(3000, () => {
